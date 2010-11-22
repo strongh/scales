02e51d4df01585363e3a75685b8dd3b80a9da42f
homer <homer.strong@gmail.com>
2010-11-22 15:03:33 -0800

    added range expander

--- R/range.r
+++ R/range.r
@@ -13,3 +13,14 @@ expand_range <- function(range, mul = 0, add = 0, zero = 0.5) {
     range + c(-1, 1) * (diff(range) * mul + add)
   }
 }
+
+# Possibly expand range
+# Convenience function for expanding a range to include a min and/or max value.
+# 
+# @arguments range of data
+# @arguments desired_min the minimum value to include
+# @arguments desired_max the maximum value to include
+# @keyword manip 
+include_in_range <- function(range, desired_min = range[1], desired_max = range[2]) {
+  base::range(range, desired_min, desired_max)
+}
